bb671ebd44140032c284f604430cd775c12d15df
Mahmoud Rady <mahmoudrady5181@gmail.com>
2025-07-06 17:15:40 +0300

    fix: update base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shoghlly-platform/', // 👈 لازم نفس اسم الريبو
-})
+  base: '/', // 👈 مهم جدًا لمواقع Vercel
+});
